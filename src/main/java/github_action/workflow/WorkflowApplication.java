5d9864c5b33ff1de593128ff013f3d958068cd7c
Admin <ayush.singh@pravisht.com>
2025-01-04 18:53:53 +0530

    first commit

---
+++ src/main/java/github_action/workflow/WorkflowApplication.java
@@ -0,0 +1,27 @@
+package github_action.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WorkflowApplication {
+
+	@GetMapping("/mapping")
+	public String welcome(){
+		return "welcome to run";
+	}
+	/*echo "# ayushgitflow" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ayushsingh666/ayushgitflow.git
+git push -u origin main*/
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github_action/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package github_action.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
